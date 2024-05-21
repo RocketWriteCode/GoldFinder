f83a78ffd5eb9a2810262b16631f46eed5278a9d
RocketCow <RaketenKuhGewehr@gmail.com>
2024-05-21 18:00:42 +0200

    Renamed Form1 to something more reasonable.

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace GoldFinder
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         /// Required designer variable.
--- MainWindow.cs
+++ MainWindow.cs
@@ -12,11 +12,11 @@ using GoldFinder.Saving;
 
 namespace GoldFinder
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
     {
         ListViewItem currentSelection;
 
-        public Form1()
+        public MainWindow()
         {
             InitializeComponent();
 
